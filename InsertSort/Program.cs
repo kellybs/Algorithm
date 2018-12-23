83286b57c8f9f5ed44c77fa21bcd81cde0befce9
kellybs <120842917@qq.com>
2018-12-23 11:24:21 +0800

    新增在排序的数组中，查找某个数字的下标

--- InsertSort/Program.cs
+++ InsertSort/Program.cs
@@ -12,9 +12,8 @@ namespace InsertSort
     class Program
     {
         static void Main(string[] args)
-        {
-
-            int[] array = { 5,15, 99, 14, 34,10,100, 66 };
+        {          
+            int[] array = { 5,15, 99, 14, 34,10,9,100, 66 };
             Console.WriteLine($"排序前的数组：{string.Join(",",array)}");
             Insert(array);
             Console.WriteLine($"排序后的数组：{string.Join(",", array)}");
@@ -28,6 +27,14 @@ namespace InsertSort
         private static void Insert(int[] array)
         {
             if (array == null || array.Length == 0) return;
+            /*
+             *  从第一个元素开始，该元素可以认为已经被排序；
+                取出下一个元素，在已经排序的元素序列中从后向前扫描；
+                如果该元素（已排序）大于新元素，将该元素移到下一位置；
+                重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+                将新元素插入到该位置后；
+                重复步骤2~5。
+             */
             int i,j,tmp;
             for (i = 1; i < array.Length; i++)
             {
---
+++ SearchOnlyOneNumber/SearchArrayIndex.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SearchOnlyOneNumber
+{
+    class SearchArrayIndex
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * 在一个已经排序的Int数组中，查找某个number，如果存在这个number，返回在数组的位置，反之返回-1
+             * 
+             */
+
+
+            int[] array = { 5, 40, 60, 102, 203 };
+
+            int number = 40;
+            int index = GetArrayIndex(array, number);
+
+            Console.WriteLine($"数字：{number}对应的下标：{index}");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 使用二分法查找
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private static int GetArrayIndex(int[] array,int numbers)
+        {
+            int result = -1;
+            if (array == null || array.Length == 0 || numbers < array[0] || numbers > array[array.Length - 1]) return result;
+
+            int low = 0, high = array.Length-1;
+            int mid;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                if (array[mid] == numbers)
+                {
+                    result = mid;
+                    break;
+                }
+                else if (array[mid] > numbers)
+                {
+                    high = mid-1;
+                }
+                else if (array[mid] < numbers)
+                {
+                    low = mid+1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
